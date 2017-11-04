5e298bbfa143c9bfa6bd26843919eb2f7e8ae71f
Tiffany Qi <tiffany19qi@gmail.com>
2017-11-04 16:58:22 -0700

    add comments

--- images/forms.py
+++ images/forms.py
@@ -1,3 +1,8 @@
+"""
+This file powers creation of all forms. You will need to modify
+the SignUpForm in order to start collecting the distinct_id.
+"""
+
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
